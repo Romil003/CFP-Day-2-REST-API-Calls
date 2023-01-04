2e691bcbcf3811836096f902f3015123f5e89e8c
Romil003 <romilghadge33@gmail.com>
2023-01-04 19:41:28 +0530

    Added : use of Get Request method and passing name as query parameter

--- src/main/java/com/bridgelabz/hellomessageapplication/Controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/hellomessageapplication/Controller/HelloRestController.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.hellomessageapplication.Controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,10 @@ public class HelloRestController {
     @GetMapping("/say")
     public String sayHello() {
         return "Hello from BridgeLabz!!!";
+    }
+
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name){
+        return "Hello " + name + " from BridgeLabz !!!";
     }
 }
